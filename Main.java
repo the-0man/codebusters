94fa0993d4fa3d72662e60340b5c45d6bf590d1a
the-0man <130107274+the-0man@users.noreply.github.com>
2024-12-05 11:01:48 -0500

    Update Main.java
    csv

--- Main.java
+++ Main.java
@@ -1,14 +1,39 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
-        String str = "hello world";
-        Cipher test = new Cipher(str);
-        System.out.println("Scrambled word: " + test.scramble());
+        String line = "";
+        String splitBy = ",";
+        String author = null;
+        String quote = null;
+        try
+        {
+            //parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader("English_Quotes.csv"));
+            int rand = ((int) (Math.random() * 1733) + 2);
+            for (int i = 1; i < 1734; i++)
+            {
+                line = br.readLine();
+                if (i == rand)   //returns a Boolean value
+                {
+                    String[] fullQuote = line.split(splitBy);    // use comma as separator
+                    author = fullQuote[0];
+                    quote = fullQuote[1];
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        Cipher test = new Cipher(quote);
+        System.out.println("Author: " + author);
+        System.out.println("Scrambled quote: " + test.scramble());
         Scanner scan = new Scanner(System.in);
         String guess = "";
-        while (!guess.equals(str))
+        while (!guess.equals(quote))
         {
             System.out.print("Guess: ");
             guess = scan.nextLine();
