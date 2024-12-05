3852d2810facb72cb45dbd60275e71135b163e15
the-0man <130107274+the-0man@users.noreply.github.com>
2024-12-05 14:39:35 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ public class Main {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
         String line = "";
-        String splitBy = ",";
+        String splitBy = "\t";
         String author = null;
         String quote = null;
         try
         {
             //parsing a CSV file into BufferedReader class constructor
-            BufferedReader br = new BufferedReader(new FileReader("English_Quotes.csv"));
+            BufferedReader br = new BufferedReader(new FileReader("English_Quotes.tsv"));
             int rand = ((int) (Math.random() * 1733) + 2);
             for (int i = 1; i < 1734; i++)
             {
@@ -28,8 +28,10 @@ public class Main {
         {
             e.printStackTrace();
         }
+        quote = quote.toLowerCase();
         Cipher test = new Cipher(quote);
         System.out.println("Author: " + author);
+        System.out.println(quote);
         System.out.println("Scrambled quote: " + test.scramble());
         Scanner scan = new Scanner(System.in);
         String guess = "";
